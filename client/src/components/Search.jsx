51d6b959a30faf22e5ed04a1a49dfcb90961b5ce
Joseph Moon <joemoondev@gmail.com>
2021-08-22 14:11:24 -0700

    Cleaned up

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 var Search = ({onSearch, handleInputChange, onBack, bak}) => (
   <div>
     {
-      bak
+      !bak
       ?
-      <button onClick={() => onBack()}><span>Back</span></button>
-      :
       <>
       <input type="text" onChange={(e) => handleInputChange(e.target.value)}></input>
       <button onClick={() => onSearch()}><span>Search</span></button>
       </>
+      :
+      <button onClick={() => onBack()}><span>Back</span></button>
     }
   </div>
 );
