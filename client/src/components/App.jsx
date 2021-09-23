c04a8ceb0f160de6d9d761c3ece19b1acc19ff6a
Joseph Moon <joemoondev@gmail.com>
2021-09-22 23:46:29 -0700

    Add Search bar functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
+import axios from 'axios';
 import Search from './Search';
+import Result from './Result';
 import MovieLists from './movies/MovieLists';
+import MovieList from './movies/MovieList';
 
-const App = () => (
-  <div>
-    <Search />
-    <MovieLists />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+    };
+    this.read = this.read.bind(this);
+  }
+
+  read(e, query) {
+    e.preventDefault();
+    const options = {
+      method: 'get',
+      url: `https://api.themoviedb.org/3/search/movie?api_key=ddf7d5d81b6abe118e7acc02334c1619&language=en-US&query=${query}&page=1&include_adult=false`
+    };
+    axios(options)
+      .then((response) => {
+        this.setState({
+          results: response.data.results,
+        });
+      })
+      .catch((err) => console.error(err));
+  }
+
+  render() {
+    const { results } = this.state;
+    return (
+      <div>
+        <Search read={this.read} />
+        {
+          results.length
+          ?
+          <Result movies={results} />
+          :
+          null
+        }
+        <MovieLists />
+      </div>
+    );
+  }
+};
 
 export default App;
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      query: '',
     };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      query: e.target.value,
+    });
   }
 
   render() {
@@ -16,6 +24,7 @@ class Search extends React.Component {
       backgroundColor: 'rgb(20,20,20)',
       padding: '0',
       margin: '0',
+      zIndex: '2',
     };
     const style2 = {
       display: 'inline-block',
@@ -25,6 +34,8 @@ class Search extends React.Component {
       top: '50%',
       transform: 'translateY(-50%)',
     };
+    const { query } = this.state;
+    const { read } = this.props;
     return (
       <ul style={style}>
         <img style={{ width: '3em' }} src="images/popcorn.png" alt="" />
@@ -38,8 +49,14 @@ class Search extends React.Component {
           To Watch
         </li>
         <li style={style2}>
-          <input type="text" />
-          <button type="submit">Search</button>
+          <input type="text" onChange={(e) => this.onChange(e)} value={query} />
+          <input type="submit" onClick={(e) =>
+            {
+              read(e, query);
+              this.setState({
+                query: '',
+              });
+            }} value="Search" />
         </li>
       </ul>
     );
