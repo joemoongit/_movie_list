97186cdfb0c2f0f13a11ff4240a4d84d9588bf52
Joseph Moon <joemoondev@gmail.com>
2021-09-25 11:30:48 -0700

    Refactor MovieList

--- client/src/components/movies/MovieList.jsx
+++ client/src/components/movies/MovieList.jsx
@@ -3,93 +3,75 @@ import axios from 'axios';
 import $ from 'jquery';
 import MovieListEntry from './MovieListEntry';
 
-class MovieList extends React.Component {
-  constructor(props) {
-    super(props);
+const MovieList = ({ list, id }) => {
 
-    this.state = {
-      movies: [],
-    };
-    this.read = this.read.bind(this);
-    this.scrollLeft = this.scrollLeft.bind(this);
-    this.scrollRight = this.scrollRight.bind(this);
-  }
+  const [ movies, setMovies ] = useState([]);
 
-  componentDidMount() {
-    this.read();
-  }
-
-  read() {
-    const { list } = this.props;
+  const read = () => {
     const options = {
       method: 'get',
       url: list.url,
     };
     axios(options)
-      .then((response) => {
-        this.setState({
-          movies: response.data.results,
-        });
-      })
-      .catch();
-  }
+      .then(response => setMovies(response.data.results))
+      .catch(err => console.error(err));
+  };
 
-  scrollLeft(id) {
+  useEffect(() => {
+    read();
+  });
+
+  const scrollLeft = (id) => {
     event.preventDefault();
     $(`#content${id}`).animate({
       scrollLeft: "-=490"
     }, "slow");
   }
 
-  scrollRight(id) {
+  const scrollRight = (id) => {
     event.preventDefault();
     $(`#content${id}`).animate({
       scrollLeft: "+=490"
     }, "slow")
   }
-
-  render() {
-    const { list } = this.props;
-    const { movies } = this.state;
-    const style = {
-      display: 'flex',
-      margin: '0.5em',
-      overflowX: 'hidden',
-      zIndex: '0',
-    };
-    const style2 = {
-      float: 'left',
-      zIndex: '1',
-      width: '1em',
-      position: 'relative',
-      top: '50%',
-      transform: 'translateY(700%)',
-    };
-    const style3 = {
-      float: 'right',
-      zIndex: '1',
-      width: '1em',
-      position: 'relative',
-      top: '50%',
-      transform: 'translateY(-900%)',
-    };
-    return (
+  const style = {
+    display: 'flex',
+    margin: '0.5em',
+    overflowX: 'hidden',
+    zIndex: '0',
+  };
+  const style2 = {
+    float: 'left',
+    zIndex: '1',
+    width: '1em',
+    position: 'relative',
+    top: '50%',
+    transform: 'translateY(700%)',
+  };
+  const style3 = {
+    float: 'right',
+    zIndex: '1',
+    width: '1em',
+    position: 'relative',
+    top: '50%',
+    transform: 'translateY(-900%)',
+  };
+  return (
+    <div>
+      <h4 style={{ marginLeft: '0.5em' }}>{list.name}</h4>
       <div>
-        <h4 style={{ marginLeft: '0.5em' }}>{list.name}</h4>
-        <div>
-          <img onClick={() => this.scrollLeft(this.props.id)} style={style2} src="./images/left.png" alt="" />
-          <div id={`content${this.props.id}`} style={style}>
-            {
-              movies.map((movie) => (
-                <MovieListEntry movie={movie} key={movie.id} />
-              ))
-            }
-          </div>
-          <img onClick={() => this.scrollRight(this.props.id)} style={style3} src="./images/right.png" alt="" />
+        <img onClick={() => scrollLeft(id)} style={style2} src="./images/left.png" alt="" />
+        <div id={`content${id}`} style={style}>
+          {
+            movies.map((movie) => (
+              <MovieListEntry movie={movie} key={movie.id} />
+            ))
+          }
         </div>
+        <img onClick={() => scrollRight(id)} style={style3} src="./images/right.png" alt="" />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default MovieList;
