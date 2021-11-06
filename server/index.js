949d5b7fe8bfad94985d8af517a7f9c595d7ff67
Joseph Moon <joemoondev@gmail.com>
2021-11-06 12:06:35 -0700

    Use cors

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res, next) => {
