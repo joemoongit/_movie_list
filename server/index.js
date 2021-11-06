4502934df01289a832ec1c0d237dc2d196cc3822
Joseph Moon <joemoondev@gmail.com>
2021-11-06 10:44:04 -0700

    Add redirect to https from http

--- server/index.js
+++ server/index.js
@@ -7,6 +7,16 @@ app.use(express.static('client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+      next();
+    }
+  });
+}
+
 const movies = require('./routes/movies');
 const tv = require('./routes/tv');
 
